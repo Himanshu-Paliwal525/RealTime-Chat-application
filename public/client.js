0abea7f7b79e47488f3f3bfcc510373652e67232
Himanshu-Paliwal525 <110467498+Himanshu-Paliwal525@users.noreply.github.com>
2023-09-09 19:00:29 +0530

    client.js

--- public/client.js
+++ public/client.js
@@ -3,35 +3,50 @@ const socket = io()
 let name;
 let textarea = document.querySelector('#inp')
 let msg_area = document.querySelector('.container')
-// let send = document.querySelector('#send')
-
+let userName = document.querySelector('#userName')
+let users = document.getElementById('tot_users')
 do {
     name = prompt('Please enter your name: ')
 } while (!name);
+userName.innerHTML = `${name}`
 
 textarea.addEventListener('keyup', (e) => {
     if (e.key == 'Enter') {
         sendMessage(e.target.value)
     }
 })
-function send(){
+
+function send() {
     sendMessage(textarea.value)
 }
+function names() {
+    appendName(name)
+    socket.emit('showName', name)
+}
+
 
 function sendMessage(message) {
     let msg = {
         user: name,
         message: message.trim()
     }
+
     // Append
     appendMessage(msg, 'right')
     textarea.value = ''
     scrollToBottom()
 
     //Send to server
-    socket.emit('message',msg)
+    socket.emit('message', msg)
 }
 
+function appendName(person) {
+    let names = document.createElement('li')
+    names.classList.add('user');
+    names.innerHTML = `${person}`
+    users.appendChild(names)
+
+}
 function appendMessage(msg, type) {
     let mainDiv = document.createElement('div')
     let className = type;
@@ -45,13 +60,40 @@ function appendMessage(msg, type) {
     msg_area.appendChild(mainDiv)
 }
 
-// Receive message
+function update(activeUsers) {
 
-socket.on('message',(msg)=>{
-    appendMessage(msg,'left')
+    while (users.hasChildNodes()) {
+        users.removeChild(users.firstChild);
+    }
+
+    for (let i = 0; i < activeUsers.length; i++) {
+        let names = document.createElement('li')
+        names.classList.add('user');
+        names.innerHTML = `${activeUsers[i]}`;
+        users.appendChild(names)
+    }
+    console.log(activeUsers)
+}
+function disconnectUser(user, activeUsers) {
+
+    console.log(`${user} has left the chat`);
+    delete activeUsers[user]; // Remove user from activeUsers
+    update(activeUsers);
+}
+
+// Receive message
+socket.on('showName', (activeUsers) => {
+    update(activeUsers);
+})
+socket.on('message', (msg) => {
+    appendMessage(msg, 'left')
     scrollToBottom()
 })
+socket.on('disconnected', (name, activeUsers) => {
+    disconnectUser(name, activeUsers);
+})
 
-function scrollToBottom(){
+
+function scrollToBottom() {
     msg_area.scrollTop = msg_area.scrollHeight
 }
